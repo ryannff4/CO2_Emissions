697dbc08c453a9f34dc5340770139ce3cb65ee0b
ryannff4 <ryannff4@gmail.com>
2021-08-10 12:39:20 -0700

    reading in csv file into a dataframe object using pandas

--- co2_visual.py
+++ co2_visual.py
@@ -1,6 +1,8 @@
 import geopandas as gpd
+import pandas as pd
 
 shapefile = 'data/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp'
+datafile = 'data/co2_data.csv'
 
 # read shapefile using geopandas
 gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
@@ -11,3 +13,6 @@ gdf.head()
 print(gdf[gdf['country'] == 'Antarctica'])
 #drop row corresponding to antarctica
 gdf = gdf.drop(gdf.index[159])
+
+# read csv file into data frame
+df = pd.read_csv(datafile, skiprows=4)  # first 4 rows of csv file are empty
