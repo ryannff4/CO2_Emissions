db42cc1fc393301fce30ff6a95cd1af034d248a4
ryannff4 <ryannff4@gmail.com>
2021-08-10 12:52:50 -0700

    merged csv dataframe with the map geodataframe on country code; implementing to view the merged data for countries for just a single year

--- co2_visual.py
+++ co2_visual.py
@@ -10,9 +10,18 @@ gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry']]
 gdf.columns = ['country', 'country_code', 'geometry']
 gdf.head()
 
-print(gdf[gdf['country'] == 'Antarctica'])
-#drop row corresponding to antarctica
+# print(gdf[gdf['country'] == 'Antarctica'])
+
+# drop row corresponding to antarctica
 gdf = gdf.drop(gdf.index[159])
 
 # read csv file into data frame
 df = pd.read_csv(datafile, skiprows=4)  # first 4 rows of csv file are empty
+
+# filter for a single year for now
+df_2016 = df[['Country Code', '2016']]  # obtain data for every country for the specified year
+# print(df_2016)
+
+# merge gdf and df
+merged = gdf.merge(df_2016, left_on='country_code', right_on='Country Code')
+# print(merged)
